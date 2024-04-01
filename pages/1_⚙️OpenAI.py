383868afb27aa387bd6037e7164f40fae6aba690
wandesai <Wan_ds@163.com>
2024-04-01 12:48:22 +0800

    second commit

--- pages/1_⚙️OpenAI.py
+++ pages/1_⚙️OpenAI.py
@@ -1,6 +1,7 @@
 import streamlit as st
 if "OPENAI_API_KEY" not in st.session_state:
     st.session_state["OPENAI_API_KEY"]=""
+
 st.set_page_config(page_title="OpenAI Settings",layout="wide")
 
 st.title("OpenAI Settings")
